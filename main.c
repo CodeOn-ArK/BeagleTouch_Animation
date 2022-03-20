e40a115d2a511e913dc0d94fb0ba50e37a0a307d
CodeOn-ArK <1930038@kiit.ac.in>
2022-03-20 18:05:26 +0530

    read update

--- main.c
+++ main.c
@@ -22,16 +22,66 @@
 /* This is the I2C slave address of TOUCH sensor */                                                                                                                                                          
 #define TOUCH_SLAVE_ADDR 0x08
 
+#define I2C_DEV_FILE    "/dev/i2c-2"
+#define BIG_BUTTON      0x00
+#define SLIDER          0x01
+
 int fd;
+int touch_init(void);
+int touch_read(uint8_t base_addr, int *pBuffer);
+int buf[2] = {0};
 
 int main(int argc, char *argv[]){
+  if(touch_init())
+    return EXIT_FAILURE;
+
+  int prev_val = 0;
+  while(1){
+    if(!touch_read(BIG_BUTTON, &buf[0]) && !touch_read(SLIDER, &buf[1])){
+      perror("Read failed\n");
+      return EXIT_FAILURE;
+    }
+
+    switch(buf[0]){
+//      case 0 : printf("-\n") ;
+//               break;
+      case 1 : printf("|\n");
+               break;
+      case 2 : printf("+\n");
+               break;
+    }
+
+    if(buf[1] < prev_val){
+      printf("2\n") ;
+    }
+
+    prev_val = buf[1];
+
+  }
+
   
   return 0;
 }
 
+int touch_init(void){
+  if((fd = open(I2C_DEV_FILE, O_RDWR)) < 0){
+   printf("%s : Unable to open %s\n", __func__, I2C_DEV_FILE);
+   return EXIT_FAILURE;
+
+  }
+
+  if(ioctl(fd, I2C_SLAVE, TOUCH_SLAVE_ADDR) < 0){
+   printf("%s : Failed to setup %s\n", __func__, "Touch Sensor");
+   return EXIT_FAILURE;
+    
+  }
+
+  return 0;
+  
+}
 
 /*read "len" many bytes from "addr" of the sensor in to the adresss indicated by "pBuffer" */
-int touch_read(uint8_t base_addr, char *pBuffer,uint32_t len)
+int touch_read(uint8_t base_addr, int *pBuffer)
 {
   int ret;
   char buf[2];
@@ -43,7 +93,7 @@ int touch_read(uint8_t base_addr, char *pBuffer,uint32_t len)
       return -1;
   }
 
-  ret = read(fd,pBuffer,len);
+  ret = read(fd,pBuffer,1);
   if(ret <= 0)
   {
       perror("read failed\n");
