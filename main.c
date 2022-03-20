d193b5589737218b3bf15d832c56b6d6231d48d7
CodeOn-ArK <1930038@kiit.ac.in>
2022-03-20 18:45:32 +0530

    header added

--- main.c
+++ main.c
@@ -1,51 +1,28 @@
-/*============================================================================
- Name        : main.c
- Author      : ArK (1930038@kiit.ac.in)
- Version     : v1.0
- Copyright   : Your copyright notice
- Description : 
- ============================================================================*/
+  /*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
+  *                                                                                       *
+  | Copyright (C) 2022 CodeOn-ArK@github                                                  |
+  *                                                                                       *
+  | This program is free software: you can redistribute it and/or modify                  |
+  * it under the terms of the GNU General Public License as published by                  *
+  | the Free Software Foundation, either version 3 of the License, or                     |
+  * (at your option) any later version.                                                   *
+  |                                                                                       |
+  * This program is distributed in the hope that it will be useful,                       *
+  | but WITHOUT ANY WARRANTY; without even the implied warranty of                        |
+  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                         *
+  | GNU General Public License for more details.                                          |
+  *                                                                                       *
+  | You should have received a copy of the GNU General Public License                     |
+  * along with this program.  If not, see <https://www.gnu.org/licenses/>.                *
+  |                                                                                       |
+  **-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*/
 
-
-#include <errno.h>
-#include <string.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <linux/i2c-dev.h>
-#include <sys/ioctl.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <stdint.h>
-
-#include <ctype.h>
-#include <termios.h>
-#include <unistd.h>
-
-//#include <io.h>
-#include "ansi/cursor.h"
-#include "ansi/screen.h"
-#include "ansi/colors.h"
-#include "graphics/common.h"
-#include "graphics/box.h"
-
-/* This is the I2C slave address of TOUCH sensor */                                                                                                                                                          
-#define TOUCH_SLAVE_ADDR 0x08
-
-#define I2C_DEV_FILE    "/dev/i2c-2"
-#define BIG_BUTTON      0x00
-#define SLIDER          0x01
+#include "main.h"
 
 int fd;
-int touch_init(void);
-int touch_read(uint8_t base_addr, int *pBuffer);
 int buf[2] = {0};
-
 struct termios orig_termios;    /* Keeps the original attributes */
 
-static inline void enable_raw_mode(void);
-static inline void disable_raw_mode(void);
 
 static inline void enable_raw_mode(void)
 {
